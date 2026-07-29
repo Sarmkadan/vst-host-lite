3c2d05b7c5d6f6d208615cfac72e73daf0db301c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 21:30:00 +0300

    tighten guard clauses in AudioGraphExtensionsJsonExtensions
    no behaviour change intended.

---
+++ src/VstHostLite.Native/AudioGraphExtensionsJsonExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VstHostLite.Native
+{
+    public class AudioGraphExtensionsJsonExtensions
+    {
+        public string Name { get; set; }
+        public List<string> Extensions { get; set; }
+    }
+}
