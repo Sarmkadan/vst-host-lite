189d86c8b1cbc0764fab00d2b581373be029a036
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-01 20:52:54 +0000

    chore: apply workspace updates

---
+++ AudioGraph.cs
@@ -0,0 +1,16 @@
+public class AudioGraph : IAudioGraph
+    {
+        public AudioGraph()
+        {
+        }
+
+        public void ConnectTo(INoiseGeneratorNode node)
+        {
+            // implementation
+        }
+
+        public void Chain(params INoiseGeneratorNode[] nodes)
+        {
+            // implementation
+        }
+    }
