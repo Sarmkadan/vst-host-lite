02d0bc1192cba324c3c7cb07fd6f7da530c3bdaa
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-03 20:41:47 +0000

    chore: apply workspace updates

---
+++ src/VstHostLite.Native/NativeModuleExtensionsJsonExtensions.cs
@@ -0,0 +1,12 @@
+public class NativeModuleExtensionsJsonExtensions
+{
+    public string GetJsonExtensions(string json)
+    {
+        if (json == null)
+        {
+            throw new ArgumentNullException(nameof(json));
+        }
+        // TO DO: implement this method
+        return "";
+    }
+}
