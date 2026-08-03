af58c6b3976ef2655bcda17d3a7749397e4a49dd
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-03 12:11:31 +0000

    chore: apply workspace updates

---
+++ src/VstHostLite.Native/NativeModuleJsonExtensionsJsonExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VstHostLite.Native
+{
+    public class NativeModuleJsonExtensionsJsonExtensions
+    {
+        public NativeModuleJsonExtensionsJsonExtensions()
+        {
+        }
+
+        public void Initialize(string param)
+        {
+        }
+    }
+}
