143dd905b78481da88e6211a728f2f64cd55b57c
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-08 03:08:13 +0000

    chore: apply workspace updates

---
+++ tests/VstHostLite.Native.Tests/AudioGraphJsonExtensionsTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using VstHostLite.Native;
+using VstHostLite.Native.Tests;
+
+namespace VstHostLite.Native.Tests
+{
+    public class AudioGraphJsonExtensionsTests
+    {
+        [Fact]
+        public void Read_AudioGraphJsonExtensions_Json_Serialization_Work()
+        {
+            // Arrange, Act, Assert
+        }
+
+        [Fact]
+        public void Write_AudioGraphJsonExtensions_Json_Serialization_Fail()
+        {
+            // Arrange, Act, Assert
+        }
+    }
+}
